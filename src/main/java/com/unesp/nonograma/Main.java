68b2dd87a47617e01d59ae16629e77aaa83d649a
vmanzoli <160800008+Sohtab@users.noreply.github.com>
2026-09-01 17:29:29 -0300

    Refactor main method to use SwingUtilities

--- src/main/java/com/unesp/nonograma/Main.java
+++ src/main/java/com/unesp/nonograma/Main.java
@@ -1,13 +1,14 @@
 package com.unesp.nonograma;
 
-
+import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
-        Tabuleiro tb = new Tabuleiro("TP", 10, 10);
-        tb.setDificuldade(2);
-        tb.gerarTabuleiroAleatorio();
-
-        TelaJogo tela = new TelaJogo(tb);
-        tela.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            Tabuleiro tb = new Tabuleiro("TP", 10, 10);
+            tb.setDificuldade(2);
+            tb.gerarTabuleiroAleatorio();
+            TelaJogo tela = new TelaJogo(tb);
+            tela.setVisible(true);
+        });
     }
 }
